95cc4b1b81999b7df1eda05e710d28ccf6472dd8
gurjan <jana.gurbanova@gmail.com>
2021-08-22 16:53:01 +0300

    added database calls

--- page_spider.py
+++ page_spider.py
@@ -1,6 +1,6 @@
 import os
 import argparse
-from utilities import url_utilities
+from utilities import url_utilities, database_utilities
 # Avoid ssl cert problem
 import ssl
 
@@ -11,9 +11,10 @@ except AttributeError:
 else:
     ssl._create_default_https_context = _create_unverified_https_context
 
+
 def main(database: str, url_list_file: str):
     big_word_list = []
-    print("we are going to work with: " + database)  # commad + D to duplicate previous line
+    print("we are going to work with: " + database)  # command + D to duplicate previous line
     print("we are going to scan: " + url_list_file)
     urls = url_utilities.load_urls_from_file(url_list_file)
     for url in urls:
@@ -21,6 +22,12 @@ def main(database: str, url_list_file: str):
         page_content = url_utilities.load_page(url=url)  # url=url - explicit name
         words = url_utilities.scrape_page(page_contents=page_content)
         big_word_list.extend(words)
+
+    # database code
+    os.chdir(os.path.dirname(__file__))
+    path = os.path.join(os.getcwd(), "words.db")
+    database_utilities.create_database(database_path=path)
+    database_utilities.save_words_to_database(database_path=path, words_list=big_word_list)
 
 
 if __name__ == "__main__":
